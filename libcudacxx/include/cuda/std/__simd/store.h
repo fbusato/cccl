54b9537375157b80e07b8ec6b8884e7825b1efc1
fbusato <fbusato@nvidia.com>
2026-04-13 15:22:13 -0700

    improve load/store functions

--- libcudacxx/include/cuda/std/__simd/store.h
+++ libcudacxx/include/cuda/std/__simd/store.h
@@ -21,11 +21,15 @@
 #  pragma system_header
 #endif // no system header
 
+#include <cuda/__cmath/pow2.h>
+#include <cuda/__memory/ptr_rebind.h>
+#include <cuda/std/__algorithm/max.h>
 #include <cuda/std/__concepts/concept_macros.h>
 #include <cuda/std/__iterator/concepts.h>
 #include <cuda/std/__iterator/distance.h>
 #include <cuda/std/__iterator/incrementable_traits.h>
 #include <cuda/std/__iterator/readable_traits.h>
+#include <cuda/std/__memory/assume_aligned.h>
 #include <cuda/std/__memory/pointer_traits.h>
 #include <cuda/std/__ranges/access.h>
 #include <cuda/std/__ranges/concepts.h>
@@ -42,6 +46,17 @@
 
 _CCCL_BEGIN_NAMESPACE_CUDA_STD_SIMD
 
+template <typename _Tp, typename _Abi, typename _Up, typename... _Flags>
+_CCCL_API constexpr void __check_store_preconditions(_Up* __ptr)
+{
+  static_assert(__is_vectorizable_v<_Up>, "range_value_t<R> must be a vectorizable type");
+  static_assert(__explicitly_convertible_to<_Up, _Tp>,
+                "value_type must satisfy explicitly-convertible-to<range_value_t<R>>");
+  static_assert(__has_convert_flag_v<_Flags...> || __is_value_preserving_v<_Tp, _Up>,
+                "Conversion from value_type to range_value_t<R> is not value-preserving; use flag_convert");
+  ::cuda::std::simd::__assert_load_store_alignment<basic_vec<_Tp, _Abi>, _Up, _Flags...>(__ptr);
+}
+
 // [simd.loadstore] helper: core partial store to pointer + count + mask
 template <typename _Tp, typename _Abi, typename _Up, typename... _Flags>
 _CCCL_API constexpr void __partial_store_to_ptr(
@@ -50,12 +65,7 @@ _CCCL_API constexpr void __partial_store_to_ptr(
   __simd_size_type __count,
   const typename basic_vec<_Tp, _Abi>::mask_type& __mask)
 {
-  static_assert(__is_vectorizable_v<_Up>, "range_value_t<R> must be a vectorizable type");
-  static_assert(__explicitly_convertible_to<_Up, _Tp>,
-                "value_type must satisfy explicitly-convertible-to<range_value_t<R>>");
-  static_assert(__has_convert_flag_v<_Flags...> || __is_value_preserving_v<_Tp, _Up>,
-                "Conversion from value_type to range_value_t<R> is not value-preserving; use flag_convert");
-  ::cuda::std::simd::__assert_load_store_alignment<basic_vec<_Tp, _Abi>, _Up, _Flags...>(__ptr);
+  ::cuda::std::simd::__check_store_preconditions<_Tp, _Abi, _Up, _Flags...>(__ptr);
   _CCCL_PRAGMA_UNROLL_FULL()
   for (__simd_size_type __i = 0; __i < basic_vec<_Tp, _Abi>::size; ++__i)
   {
@@ -64,6 +74,56 @@ _CCCL_API constexpr void __partial_store_to_ptr(
       __ptr[__i] = static_cast<_Up>(__v[__i]);
     }
   }
+}
+
+template <typename _Tp, typename _Abi, typename _Up, typename... _Flags>
+_CCCL_API constexpr void __full_store_to_ptr(const basic_vec<_Tp, _Abi>& __v, _Up* __ptr)
+{
+  ::cuda::std::simd::__check_store_preconditions<_Tp, _Abi, _Up, _Flags...>(__ptr);
+  constexpr auto __simd_size = basic_vec<_Tp, _Abi>::size;
+
+  if constexpr (__has_aligned_flag_v<_Flags...> || __has_overaligned_flag_v<_Flags...>)
+  {
+    _CCCL_IF_NOT_CONSTEVAL
+    {
+      constexpr auto __base_alignment = alignment_v<basic_vec<_Tp, _Abi>, _Up>;
+      constexpr auto __ptr_alignment  = ::cuda::std::max(__base_alignment, __overaligned_value_v<_Flags...>);
+      constexpr auto __data_size      = __simd_size * sizeof(_Up);
+
+      _Up __tmp[__simd_size];
+      _CCCL_PRAGMA_UNROLL_FULL()
+      for (__simd_size_type __i = 0; __i < __simd_size; ++__i)
+      {
+        __tmp[__i] = static_cast<_Up>(__v[__i]);
+      }
+      // vectorized store to pointer
+      if constexpr (__is_cuda_vectoriazable_v<_Up> && __ptr_alignment >= __data_size)
+      {
+        struct alignas(__data_size) __aligned_t
+        {
+          char __data[__data_size];
+        };
+        // nvcc performance bug: memcpy to pointer could not be vectorized
+        auto __aligned_ptr = ::cuda::ptr_rebind<__aligned_t>(__ptr);
+        __aligned_t __data;
+        ::cuda::std::memcpy(&__data, &__tmp, sizeof(__tmp));
+        *::cuda::std::assume_aligned<__ptr_alignment>(__aligned_ptr) = __data;
+      }
+      // rely on compiler vectorization
+      else
+      {
+        auto __aligned_ptr = ::cuda::std::assume_aligned<__ptr_alignment>(__ptr);
+        _CCCL_PRAGMA_UNROLL_FULL()
+        for (__simd_size_type __i = 0; __i < __simd_size; ++__i)
+        {
+          __aligned_ptr[__i] = __tmp[__i];
+        }
+      }
+      return;
+    }
+  }
+  ::cuda::std::simd::__partial_store_to_ptr<_Tp, _Abi, _Up, _Flags...>(
+    __v, __ptr, __simd_size, typename basic_vec<_Tp, _Abi>::mask_type(true));
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -162,8 +222,9 @@ _CCCL_API constexpr void unchecked_store(
   const basic_vec<_Tp, _Abi>& __v,
   _Range&& __r,
   const typename basic_vec<_Tp, _Abi>::mask_type& __mask,
-  flags<_Flags...> __f = {})
+  flags<_Flags...> = {})
 {
+  using _Up = ranges::range_value_t<_Range>;
   if constexpr (__has_static_size<_Range>)
   {
     static_assert(__static_range_size_v<_Range> >= basic_vec<_Tp, _Abi>::size(),
@@ -171,17 +232,25 @@ _CCCL_API constexpr void unchecked_store(
   }
   _CCCL_ASSERT(::cuda::std::cmp_greater_equal(::cuda::std::ranges::size(__r), __v.size),
                "unchecked_store requires ranges::size(r) >= V::size()");
-  ::cuda::std::simd::partial_store(__v, ::cuda::std::forward<_Range>(__r), __mask, __f);
+  ::cuda::std::simd::__partial_store_to_ptr<_Tp, _Abi, _Up, _Flags...>(
+    __v, ::cuda::std::ranges::data(__r), basic_vec<_Tp, _Abi>::size, __mask);
 }
 
 // unchecked_store: range, no mask
 _CCCL_TEMPLATE(typename _Tp, typename _Abi, typename _Range, typename... _Flags)
 _CCCL_REQUIRES(ranges::contiguous_range<_Range> _CCCL_AND ranges::sized_range<_Range> _CCCL_AND
                  __explicitly_convertible_to<ranges::range_value_t<_Range>, _Tp>)
-_CCCL_API constexpr void unchecked_store(const basic_vec<_Tp, _Abi>& __v, _Range&& __r, flags<_Flags...> __f = {})
+_CCCL_API constexpr void unchecked_store(const basic_vec<_Tp, _Abi>& __v, _Range&& __r, flags<_Flags...> = {})
 {
-  ::cuda::std::simd::unchecked_store(
-    __v, ::cuda::std::forward<_Range>(__r), typename basic_vec<_Tp, _Abi>::mask_type(true), __f);
+  using _Up = ranges::range_value_t<_Range>;
+  if constexpr (__has_static_size<_Range>)
+  {
+    static_assert(__static_range_size_v<_Range> >= basic_vec<_Tp, _Abi>::size(),
+                  "unchecked_store requires ranges::size(r) >= V::size()");
+  }
+  _CCCL_ASSERT(::cuda::std::cmp_greater_equal(::cuda::std::ranges::size(__r), __v.size),
+               "unchecked_store requires ranges::size(r) >= V::size()");
+  ::cuda::std::simd::__full_store_to_ptr<_Tp, _Abi, _Up, _Flags...>(__v, ::cuda::std::ranges::data(__r));
 }
 
 // unchecked_store: iterator + count, masked
@@ -192,19 +261,23 @@ _CCCL_API constexpr void unchecked_store(
   _Ip __first,
   iter_difference_t<_Ip> __n,
   const typename basic_vec<_Tp, _Abi>::mask_type& __mask,
-  flags<_Flags...> __f = {})
+  flags<_Flags...> = {})
 {
+  using _Up = iter_value_t<_Ip>;
   _CCCL_ASSERT(::cuda::std::cmp_greater_equal(__n, __v.size), "unchecked_store requires n >= V::size()");
-  ::cuda::std::simd::partial_store(__v, __first, __n, __mask, __f);
+  ::cuda::std::simd::__partial_store_to_ptr<_Tp, _Abi, _Up, _Flags...>(
+    __v, ::cuda::std::to_address(__first), basic_vec<_Tp, _Abi>::size, __mask);
 }
 
 // unchecked_store: iterator + count, no mask
 _CCCL_TEMPLATE(typename _Tp, typename _Abi, typename _Ip, typename... _Flags)
 _CCCL_REQUIRES(contiguous_iterator<_Ip> _CCCL_AND __explicitly_convertible_to<iter_value_t<_Ip>, _Tp>)
 _CCCL_API constexpr void
-unchecked_store(const basic_vec<_Tp, _Abi>& __v, _Ip __first, iter_difference_t<_Ip> __n, flags<_Flags...> __f = {})
+unchecked_store(const basic_vec<_Tp, _Abi>& __v, _Ip __first, iter_difference_t<_Ip> __n, flags<_Flags...> = {})
 {
-  ::cuda::std::simd::unchecked_store(__v, __first, __n, typename basic_vec<_Tp, _Abi>::mask_type(true), __f);
+  using _Up = iter_value_t<_Ip>;
+  _CCCL_ASSERT(::cuda::std::cmp_greater_equal(__n, __v.size), "unchecked_store requires n >= V::size()");
+  ::cuda::std::simd::__full_store_to_ptr<_Tp, _Abi, _Up, _Flags...>(__v, ::cuda::std::to_address(__first));
 }
 
 // unchecked_store: iterator + sentinel, masked
@@ -216,21 +289,25 @@ _CCCL_API constexpr void unchecked_store(
   _Ip __first,
   _Sp __last,
   const typename basic_vec<_Tp, _Abi>::mask_type& __mask,
-  flags<_Flags...> __f = {})
+  flags<_Flags...> = {})
 {
+  using _Up = iter_value_t<_Ip>;
   _CCCL_ASSERT(::cuda::std::cmp_greater_equal(::cuda::std::distance(__first, __last), __v.size),
                "unchecked_store requires distance(first, last) >= V::size()");
-  ::cuda::std::simd::partial_store(__v, __first, __last, __mask, __f);
+  ::cuda::std::simd::__partial_store_to_ptr<_Tp, _Abi, _Up, _Flags...>(
+    __v, ::cuda::std::to_address(__first), basic_vec<_Tp, _Abi>::size, __mask);
 }
 
 // unchecked_store: iterator + sentinel, no mask
 _CCCL_TEMPLATE(typename _Tp, typename _Abi, typename _Ip, typename _Sp, typename... _Flags)
 _CCCL_REQUIRES(contiguous_iterator<_Ip> _CCCL_AND sized_sentinel_for<_Sp, _Ip> _CCCL_AND
                  __explicitly_convertible_to<iter_value_t<_Ip>, _Tp>)
-_CCCL_API constexpr void
-unchecked_store(const basic_vec<_Tp, _Abi>& __v, _Ip __first, _Sp __last, flags<_Flags...> __f = {})
+_CCCL_API constexpr void unchecked_store(const basic_vec<_Tp, _Abi>& __v, _Ip __first, _Sp __last, flags<_Flags...> = {})
 {
-  ::cuda::std::simd::unchecked_store(__v, __first, __last, typename basic_vec<_Tp, _Abi>::mask_type(true), __f);
+  using _Up = iter_value_t<_Ip>;
+  _CCCL_ASSERT(::cuda::std::cmp_greater_equal(::cuda::std::distance(__first, __last), __v.size),
+               "unchecked_store requires distance(first, last) >= V::size()");
+  ::cuda::std::simd::__full_store_to_ptr<_Tp, _Abi, _Up, _Flags...>(__v, ::cuda::std::to_address(__first));
 }
 
 _CCCL_END_NAMESPACE_CUDA_STD_SIMD
